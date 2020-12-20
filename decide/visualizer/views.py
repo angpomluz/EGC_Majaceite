f6aafcfe81e08946f64f73261541004d37f64781
Angel <angelpomaresus@gmail.com>
2020-12-20 08:59:37 +0000

    Grafica porcentaje votos de censados se carga desde modelo

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -2,6 +2,8 @@ import json
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.http import Http404
+from census.models import Census
+from store.models import Vote
 
 from base import mods
 
@@ -19,8 +21,18 @@ class VisualizerView(TemplateView):
         context['numvotos']=34
         context['porcenso_labels']=['porcentaje censo']
         context['porcenso_data']=[30]
-        context['censados']=censados
-
+        num_censed = Census.objects.filter(voting_id=vid).count()
+        num_voted = Vote.objects.filter(voting_id=vid).count()
+       
+        #Percent of censed people who voted
+        percent_voted = int(num_voted*100/num_censed)
+        #Percent of censed people who not voted
+        percent_not_voted = 100 - percent_voted
+        
+        context['num_censed']=num_censed
+        context['num_voted'] = num_voted
+        context['percent_voted']=[percent_voted,percent_not_voted]
+        
         try:
             r = mods.get('voting', params={'id': vid})
             context['voting'] = json.dumps(r[0])
