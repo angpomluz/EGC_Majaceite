47b435420d70f20452b778a2868d0ee83832e682
juapormon <56033821+juapormon@users.noreply.github.com>
2021-01-06 20:25:23 +0100

    new access token

--- decide/actions/views.py
+++ decide/actions/views.py
@@ -10,7 +10,7 @@ from base import mods
 import visualizer.views
 from voting.models import *
 
-BOT_USER_ACCESS_TOKEN= 'xoxb-1541218143492-1535266194915-ohWgxsxTFTlKeqrn2GlLWVX2'
+BOT_USER_ACCESS_TOKEN= 'xoxb-1541218143492-1535266194915-I7k51DFDg3HK8CKZjtsaNnrV'
 VERIFICATION_TOKEN='yi1ydbfn1xMrjJ1YAVH7MVUz'
 
 @csrf_exempt
