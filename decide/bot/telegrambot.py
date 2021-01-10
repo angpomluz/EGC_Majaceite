02d514ecf3baf63d87667b364ba86983b3c51572
fragarroa <fragarroa@alum.us.es>
2021-01-10 13:25:38 +0100

    /help added, /start modified-Telegram bot commands

--- decide/bot/telegrambot.py
+++ decide/bot/telegrambot.py
@@ -18,28 +18,30 @@ logger = logging.getLogger(__name__)
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    context.bot.sendMessage(update.message.chat_id, text='Hi!')
+    context.bot.sendMessage(
+        update.message.chat_id, text='¡Hola! si no sabes cómo funciona el bot, prueba a mandarme /help')
 
 
-# def help(update, context):
-#     context.bot.sendMessage(update.message.chat_id, text='Help!')
+def help(update, context):
+    context.bot.sendMessage(update.message.chat_id, text='Los comandos disponibles son: /start (saludo), /help (ayuda, muestra los comandos disponibles), /voting *votingID* (muestra el resultado de la votación que se le pida. IMPORTANTE la votación debe estar con el tally para que el bot la reconozca. Ej: /voting 1), /error (muestra la traza del error en caso de que hubiese alguno)')
 
 
 # method for the custom command - gets a voting by its ID and puts it into a text
 def getVoting(votingId):
     objeto_voting = Voting.objects.get(id=votingId)
     something = objeto_voting.postproc
-    listed_values=[]
+    listed_values = []
     for s in something:
-        values_list=[]
+        values_list = []
         for v in s.values():
             values_list.append(v)
         listed_values.append(values_list)
     options_number = len(listed_values)
     unmensaje = "Estos son los resultados de la votacion %s:\n" % votingId
-    i=0
+    i = 0
     while i < options_number:
-        unmensaje = unmensaje + "-->Opcion %d: (%s) -- Votos obtenidos: (%s)\n\n" % (i, listed_values[i][2], listed_values[i][3])
+        unmensaje = unmensaje + "-->Opcion %d: (%s) -- Votos obtenidos: (%s)\n\n" % (
+            i, listed_values[i][2], listed_values[i][3])
         i = i+1
     return unmensaje
 
@@ -60,12 +62,13 @@ def main():
     # Default dispatcher (this is related to the first bot in settings.TELEGRAM_BOT_TOKENS)
     # dp = DjangoTelegramBot.dispatcher
     # To get Dispatcher related to a specific bot
-    dp = DjangoTelegramBot.getDispatcher('1478165863:AAGTc2kVAoGTI1-pZck4ZvkNYho2ldB_NX8')     #get by bot token
+    dp = DjangoTelegramBot.getDispatcher(
+        '1478165863:AAGTc2kVAoGTI1-pZck4ZvkNYho2ldB_NX8')  # get by bot token
     # dp = DjangoTelegramBot.getDispatcher('BOT_n_username')  #get by bot username
-    
+
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
-    # dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("help", help))
 
     # custom command
     dp.add_handler(CommandHandler("voting", voting))
