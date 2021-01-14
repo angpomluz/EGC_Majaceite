d81199f9c549b9af6fffecde3fec2502546491c0
Angel <angelpomaresus@gmail.com>
2021-01-14 11:44:47 +0000

    arreglo error testSelenium

--- decide/testSelenium.py
+++ decide/testSelenium.py
@@ -6,6 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 options = webdriver.FirefoxOptions()
 options.headless = True
 driver = webdriver.Firefox(options=options)
-driver.get("http://localhost:8000/visualizer/77/")
+driver.get("https://www.google.com/")
 print('Title: %s' % driver.title)
 driver.quit()
