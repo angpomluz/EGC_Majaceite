cd82ff96fccf6ee41709bcf0c3ed65b4d3e38461
Angel <angelpomaresus@gmail.com>
2021-01-14 00:40:41 +0000

    test corregidos

--- decide/actions/tests.py
+++ decide/actions/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
 from django.conf import settings
 from base.tests import BaseTestCase
+import slack
 
 class ActionModelTest(BaseTestCase):
 
     def test_response_Bot_notfromslack(self):
-        data = {} #El campo action es requerido en la request
+        data = {} 
         self.login()
-        response = self.client.get('/actions/event/hook', data, format= 'json')
+        response = self.client.post('/actions/event/hook', data, format= 'json')
         self.assertEquals(response.status_code, 301)
